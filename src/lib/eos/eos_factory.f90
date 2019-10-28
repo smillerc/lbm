7ce4762c1b29e53638008f0de8cb093b8584a518
Miller, Samuel <smil@lle.rochester.edu>
2019-10-28 16:50:09 -0400

    Added empty factories (EOS and streaming)

---
+++ src/lib/eos/eos_factory.f90
@@ -0,0 +1,6 @@
+module mod_eos_factory
+  implicit none
+
+contains
+
+end module mod_eos_factory
---
+++ src/lib/streaming_operators/streaming_operator_factory.f90
@@ -0,0 +1,6 @@
+module mod_streaming_operator_factory
+  implicit none
+
+contains
+
+end module mod_streaming_operator_factory
